b08c5e1b3239f33e944947b233b93c2ae3d535c3
Tomsen3 <tom.jansen@singende-krankenhaeuser.de>
2026-05-05 11:42:05 +0200

    Release Innenpause 3.6

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// sw.js - stabilisierte Version (v3.5)
+// sw.js - stabilisierte Version (v3.6)
 
-const CACHE_NAME = 'innenpause-v3-5';
+const CACHE_NAME = 'innenpause-v3-6';
 const CACHE_PREFIX = 'innenpause-';
 
 const SHELL = [
